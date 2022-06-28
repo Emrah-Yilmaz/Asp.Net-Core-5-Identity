207a378420925e2f907628677f39184e8db3332c
Emrah Yılmaz <emrahyilmaz57@outlook.com.tr>
2022-06-28 17:22:46 +0300

    update

---
+++ Asp.Net-Core-Identity/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_Identity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
