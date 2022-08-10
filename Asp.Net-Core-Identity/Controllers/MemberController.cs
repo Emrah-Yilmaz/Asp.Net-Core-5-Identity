c7be2dfb8e5996ae206a9a11780194ab9b64064e
Emrah Yılmaz <emrahyilmaz57@outlook.com.tr>
2022-08-10 14:36:07 +0300

    update

--- Asp.Net-Core-Identity/Controllers/MemberController.cs
+++ Asp.Net-Core-Identity/Controllers/MemberController.cs
@@ -4,6 +4,7 @@ using Mapster;
 using Microsoft.AspNetCore.Identity;
 using Asp.Net_Core_Identity.Models;
 using Asp.Net_Core_Identity.ViewModel;
+using System.Threading.Tasks;
 
 namespace Asp.Net_Core_Identity.Controllers
 {
@@ -25,6 +26,44 @@ namespace Asp.Net_Core_Identity.Controllers
             UserViewModel userView = user.Adapt<UserViewModel>();
             return View(userView);
         }
+        public IActionResult Edit()
+        {
+            AppUser user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+            UserViewModel userView = user.Adapt<UserViewModel>();
+            return View(userView);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserViewModel userViewModel)
+        {
+            ModelState.Remove("Password");
+            if (ModelState.IsValid)
+            {
+
+                AppUser user     =   await _userManager.FindByNameAsync(User.Identity.Name);
+                user.UserName    =   userViewModel.UserName;
+                user.PhoneNumber =   userViewModel.PhoneNumber;
+                user.Email       =   userViewModel.Email;
+
+                IdentityResult result = await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.UpdateSecurityStampAsync(user);
+                    await _signInManager.SignOutAsync();
+                    await _signInManager.SignInAsync(user, true);
+                    ViewBag.success = true;
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+
+            return View(userViewModel);
+        }
         public IActionResult ChangePassword()
         {
             return View();
@@ -35,34 +74,40 @@ namespace Asp.Net_Core_Identity.Controllers
             if (ModelState.IsValid)
             {
                 AppUser user = _userManager.FindByNameAsync(User.Identity.Name).Result;
-                
-                    bool exist = _userManager.CheckPasswordAsync(user, passwordChange.PasswordOld).Result;
-                    if (exist)
+
+                bool exist = _userManager.CheckPasswordAsync(user, passwordChange.PasswordOld).Result;
+                if (exist)
+                {
+                    IdentityResult result = _userManager.ChangePasswordAsync(user, passwordChange.PasswordOld, passwordChange.PasswordNew).Result;
+                    if (result.Succeeded)
                     {
-                        IdentityResult result = _userManager.ChangePasswordAsync(user, passwordChange.PasswordOld, passwordChange.PasswordNew).Result;
-                        if (result.Succeeded)
-                        {
                         _userManager.UpdateSecurityStampAsync(user);
                         _signInManager.SignOutAsync();
                         _signInManager.PasswordSignInAsync(user, passwordChange.PasswordNew, true, false);
-                            ViewBag.success = true;
+                        ViewBag.success = true;
 
-                        }
-                        else
-                        {
-                            foreach (var item in result.Errors)
-                            {
-                                ModelState.AddModelError("", item.Description);
-                            }
-                        }
                     }
                     else
                     {
-                        ModelState.AddModelError("", "Eski şifreniz yanlış");
+                        foreach (var item in result.Errors)
+                        {
+                            ModelState.AddModelError("", item.Description);
+                        }
                     }
-                
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Eski şifreniz yanlış");
+                }
+
             }
             return View(passwordChange);
+        }
+
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
